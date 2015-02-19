07dc4babab03e419fcab14ba1f5c8a9ace0ab71a
Apple Macintosh <Admin@MacBook-Air.local>
2015-02-19 11:40:01 +0700

    Init yii generator module

---
+++ AutoloadExample.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace yii3ds\gii;
+
+/**
+ * This is just an example.
+ */
+class AutoloadExample extends \yii\base\Widget
+{
+    public function run()
+    {
+        return "Hello!";
+    }
+}
